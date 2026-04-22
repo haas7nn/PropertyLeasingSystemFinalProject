bb2d6b3bc833293e53d1efeb6fdea63e36abad49
znbayoob <znbayoob@gmail.com>
2026-04-22 11:31:01 +0300

    PropertyManager model

---
+++ PropertyManagement.API/Models/PropertyManager.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PropertyManagement.API.Models
+{
+    // role C user type with full access to all system features
+    // this class is intentionally empty because all fields come from IdentityUser
+    public class PropertyManager : IdentityUser
+    {
+
+    }
+}
