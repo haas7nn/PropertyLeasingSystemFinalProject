13cdd88837463c7a29ca1ff451134badee96d870
haas7nn <hassan.subject2@gmail.com>
2026-05-05 17:26:50 +0300

    login request and response DTOs

---
+++ PropertyManagement.API/DTOs/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyManagement.API.DTOs
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new List<string>();
+        public DateTime Expiration { get; set; }
+    }
+}
